576edc9d2e23275c2e1fbbbc3e325f7ffc0994d2
POD666 <v.v.danyliuk@gmail.com>
2018-01-19 12:56:28 +0200

    Chabges from nephila:feature/django_20 + some fixes

--- filer/migrations/0001_initial.py
+++ filer/migrations/0001_initial.py
@@ -32,7 +32,7 @@ class Migration(migrations.Migration):
             name='ClipboardItem',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('clipboard', models.ForeignKey(verbose_name='clipboard', to='filer.Clipboard')),
+                ('clipboard', models.ForeignKey(verbose_name='clipboard', to='filer.Clipboard', on_delete=models.CASCADE)),
             ],
             options={
                 'verbose_name': 'clipboard item',
